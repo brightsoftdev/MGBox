4a905bb4a27e795001fda7afd187fa42cf2b30a6
matt <matt@bigpaua.com>
2012-05-24 07:43:58 +1200

    Initial Commit

---
+++ MGBox Demo App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MGBox Demo App
+//
+//  Created by Matt Greenfield on 24/05/12.
+//  Copyright (c) 2012 Digital Nomad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MGBox Demo App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MGBox Demo App
+//
+//  Created by Matt Greenfield on 24/05/12.
+//  Copyright (c) 2012 Digital Nomad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
